fd6e243c4dd72298632cdf5fb78386a4b8ef6538
kimyang-sun <berman532@gmail.com>
2021-02-08 01:31:50 +0900

    데모 수정

--- src/app.ts
+++ src/app.ts
@@ -38,36 +38,16 @@ class App {
 
     // demo
     this.items.addChild(
-      new VideoItem('첼시, 그 10년의 역사', 'https://youtu.be/JzYAFeYO6cY')
+      new NoteItem(
+        '타입스크립트 객체지향',
+        `링크 사진, 유튜브 비디오, 텍스트를 추가할 수 있습니다. 🙂`
+      )
     );
     this.items.addChild(
       new ImageItem('랜덤 이미지', 'https://picsum.photos/560/260')
     );
     this.items.addChild(
-      new ImageItem('랜덤 이미지 두번째', 'http://loremflickr.com/560/260')
-    );
-    this.items.addChild(
-      new NoteItem(
-        '아무 의미없는 문장',
-        `간에 같이, 오아이스도 풀이 위하여, 놀이 안고, 것이다.
-        피가 청춘을 따뜻한 청춘이 트고, 뿐이다.
-        예수는 가진 따뜻한 때에, 피고 옷을 있으며, 보이는 대한 아니다.
-        보내는 그들의 있는 뜨거운지라, 가는 때문이다.
-        청춘 이 인간의 그들은 꽃이 피어나기 할지니, 이것이다.
-        뛰노는 끓는 청춘의 보이는 인간에 고동을 것이다.
-        영락과 되려니와, 두손을 피어나는 스며들어 교향악이다.
-        뜨고, 피고, 군영과 때문이다. 얼음과 있는 우리 싸인 같이, 천고에 부패뿐이다.
-        같은 얼마나 관현악이며, 영원히 열락의 뜨고, 그것은 생생하며, 이것이다.`
-      )
-    );
-    this.items.addChild(
-      new ImageItem(
-        '랜덤 이미지 세번째',
-        'https://source.unsplash.com/random/560x260'
-      )
-    );
-    this.items.addChild(
-      new VideoItem('Bayern Munich vs Chelsea', 'https://youtu.be/khZC_L7Yv7s')
+      new VideoItem('첼시, 그 10년의 역사', 'https://youtu.be/JzYAFeYO6cY')
     );
   }
 
